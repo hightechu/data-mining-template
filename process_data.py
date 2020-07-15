341ca6bfa8e0b465bcf174b1933e7040f906c7c7
Cairo Sanders <45152371+cairosanders@users.noreply.github.com>
2020-07-14 18:04:51 -0700

    Delete process_data.py

--- process_data.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-from sklearn.tree import DecisionTreeClassifier
-from sklearn.model_selection import train_test_split
-
-movie_data = pd.read_csv('rotten_tomatoes_top_movies.csv')
-print(movie_data.head(10))
